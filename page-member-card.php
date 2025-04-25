baff5235f6da189de82885ab7510ced02dfb0f1a
Christian Abegg <christian.abegg@zuehlke.com>
2025-04-25 22:06:38 +0200

    generate qr code

--- page-member-card.php
+++ page-member-card.php
@@ -2,13 +2,32 @@
 /**
  * Template Name: Member Card
  */
+function add_qrcodejs_script() {
+  wp_enqueue_script('qrcodejs', 
+    get_theme_file_uri() . '/lib/qrcodejs/qrcode.min.js',
+    array(), 
+    false,
+    false);
+}
+
+add_action('wp_enqueue_scripts', 'add_qrcodejs_script');
+ 
 get_header();
 
-$user_id = get_current_user_id();
-$field_value = get_field('member_id', 'user_' . $user_id);
+the_title('<h1>', '</h1>');
 
-echo '<h2>User Field Value:</h2>';
-echo esc_html($field_value);
+$user_id = get_current_user_id();
+if (!$user_id) {
+  echo "Bitte einloggen.";
+} else { 
+  $field_value = get_field('member_id', 'user_' . $user_id);
+  if (!$field_value) {
+    echo "Für dich ist noch keine Mitgliederkarte erfasst.";
+  } else {
+    echo "<div id='qrcode'></div>";
+    echo "<script type='text/javascript'>new QRCode(document.getElementById('qrcode'), '" . $field_value . "');</script>";
+  }
+}
 
 get_footer();
 ?>
